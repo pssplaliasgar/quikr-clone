1af947dcc63a0c4a15878d8547d6664b7e5252f7
Aliasgar Mandviwala <Aliasgar.Mandviwala@auction-cloud.com>
2026-05-01 14:30:30 +0530

    refactor: update .gitignore paths and reorder imports in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './store'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import { store } from './store'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
